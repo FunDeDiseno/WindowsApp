8276ed7291ae5907779fd339f2505196976283ba
Miguel Pérez García <miguelpg_95@hotmail.com>
2016-05-02 23:43:03 -0500

    new functionalities

--- federacionHemofiliaPacienteApp/federacionHemofiliaPacienteApp/Views/RegisterApp.xaml.cs
+++ federacionHemofiliaPacienteApp/federacionHemofiliaPacienteApp/Views/RegisterApp.xaml.cs
@@ -14,13 +14,13 @@ using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 using System.Net;
 using Newtonsoft.Json;
-using Newtonsoft;
 
 namespace federacionHemofiliaPacienteApp
 {
     public sealed partial class RegisterApp : Page
     {
         private string Id;
+        private WebRequest request;
         public RegisterApp()
         {
             this.InitializeComponent();
@@ -33,5 +33,55 @@ namespace federacionHemofiliaPacienteApp
             localSettings.Values["UserId"] = Id;
             base.OnNavigatedTo(e);
         }
+
+        private async void PostUser()
+        {
+            request = (HttpWebRequest)WebRequest.Create("http://localhost:5000/paciente/registerApliacion/");
+            request.Method = "POST";
+            request.ContentType = "application/json";
+            using (var streamWriter = new StreamWriter(await request.GetRequestStreamAsync()))
+            {
+                var aplicacion = new AplicacionVM
+                {
+                    nuevaAplicacion = new Aplicacion
+                    {
+                        cantidad = Convert.ToInt16(cantidad.Text),
+                        fecha = DateTime.Now
+                    },
+                    userId = Id
+                };
+
+                string json = JsonConvert.SerializeObject(aplicacion);
+                streamWriter.Write(json);
+                streamWriter.Flush();
+                streamWriter.Dispose();
+            }
+
+            var httpResponse = await request.GetResponseAsync();
+        }
+
+        private void appBtn_Click(object sender, RoutedEventArgs e)
+        {
+            PostUser();
+        }
+
+        private void closeSession_Click(object sender, RoutedEventArgs e)
+        {
+            var localSettings = Windows.Storage.ApplicationData.Current.LocalSettings;
+            localSettings.Values["UserId"] = null;
+            this.Frame.Navigate(typeof(MainPage), null);
+        }
+    }
+
+    class AplicacionVM
+    {
+        public Aplicacion nuevaAplicacion { get; set; }
+        public string userId { get; set; }
+    }
+
+    class Aplicacion
+    {
+        public int cantidad { get; set; }
+        public DateTime fecha { get; set; }
     }
 }
